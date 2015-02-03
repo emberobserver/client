59d501ac50b03e2a7e1e73a008d7fdcf7e778abd
Katie Gengler <katie@kmg.io>
2015-02-03 00:06:52 -0500

    Add ago heloer

--- Brocfile.js
+++ Brocfile.js
@@ -16,6 +16,5 @@ var app = new EmberApp();
 // modules that you would like to import into your application
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
-app.import('bower_components/timeago/jquery.timeago.js');
 
 module.exports = app.toTree();
