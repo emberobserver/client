8f484455cb321b8d0a89ac02d205ef82baf5fa15
Phil Gengler <phil@pgengler.net>
2015-02-09 22:19:33 -0500

    Generate custom jQuery UI build with only autocomplete and dependencies and use in place of the full thing

--- Brocfile.js
+++ Brocfile.js
@@ -17,4 +17,7 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
+app.import('vendor/jquery-ui.js');
+app.import('vendor/jquery-ui.theme.css');
+
 module.exports = app.toTree();
