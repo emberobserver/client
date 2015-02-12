ed33a4cd4576d9f53c27cd982d36c10f7fa4a05f
Katie Gengler <katie@kmg.io>
2015-02-12 01:35:32 -0500

    Add segment.io

--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,9 @@ module.exports = function(environment) {
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
+    segment: {
+      LOG_EVENT_TRACKING: true
+    },
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
