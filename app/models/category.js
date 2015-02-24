0799a717a4feced42c607ef9da7b79cc5a08df48
Katie Gengler <katie@kmg.io>
2015-02-23 20:49:18 -0500

    Addons in categorys but not parent categories

--- app/models/category.js
+++ app/models/category.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 var attr = DS.attr;
@@ -9,5 +10,10 @@ export default DS.Model.extend({
   description: attr('string'),
   addons: hasMany('addon', {async: true}),
   parent: belongsTo('category'),
-  subcategories: hasMany('category', {inverse: 'parent'})
+  subcategories: hasMany('category', {inverse: 'parent'}),
+  addonCount: function(){
+    return this.get('subcategories').mapBy('addons.length').reduce(function(categoryA, categoryB) {
+      return categoryA + categoryB;
+    }, this.get('addons.length'));
+  }.property('addons.length', 'subcategories.@each.addons.length')
 });
