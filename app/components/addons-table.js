b13b173039f7fb72af8aba43aedfa7bb060fa342
Phil Gengler <phil@pgengler.net>
2015-03-01 22:50:13 -0500

    Make sorting be a feature of the categories.show controller instead of the addon-table component. Improve styling.

--- app/components/addons-table.js
+++ app/components/addons-table.js
@@ -1,22 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  sortKey: 'score',
-  sortedAddons: Ember.computed.sort('addons', 'addonSorting'),
-
   actions: {
     showAddon: function(addon){
       this.sendAction("showAddon", addon);
     }
-  },
-
-  addonSorting: function() {
-    var sortKeyMapping = {
-      'latestVersionDate': 'latestVersionDate:desc',
-      'name': 'name:asc',
-      'score': 'score:desc',
-    };
-    var sortKey = sortKeyMapping[ this.get('sortKey') ] || 'score:desc';
-    return [ sortKey ];
-  }.property('sortKey')
+  }
 });
--- app/controllers/categories/show.js
+++ app/controllers/categories/show.js
@@ -5,8 +5,19 @@ export default Ember.Controller.extend({
 		addonSortKey: 'sort'
 	},
 	addonSortKey: 'score',
+	sortedAddons: Ember.computed.sort('model.addons', 'addonSorting'),
 
 	hasSubcategories: function() {
 		return this.get('model.subcategories.length') > 0;
-	}.property('model.subcategories')
+	}.property('model.subcategories'),
+
+	addonSorting: function() {
+		var sortKeyMapping = {
+			'latestVersionDate': 'latestVersionDate:desc',
+			'name': 'name:asc',
+			'score': 'score:desc',
+		};
+		var sortKey = sortKeyMapping[ this.get('addonSortKey') ] || 'score:desc';
+		return [ sortKey ];
+	}.property('addonSortKey')
 });
