060678a1b872b3fc88374548d9b58891a316daa6
Phil Gengler <phil@pgengler.net>
2015-03-01 22:06:24 -0500

    Add ability to change sort key (score or name only, so far)

--- app/components/addons-table.js
+++ app/components/addons-table.js
@@ -1,12 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  addonSorting: [ 'score:desc' ],
+  sortKey: 'score:desc',
   sortedAddons: Ember.computed.sort('addons', 'addonSorting'),
 
   actions: {
     showAddon: function(addon){
       this.sendAction("showAddon", addon);
     }
-  }
+  },
+
+  addonSorting: function() {
+    return [ this.get('sortKey') ];
+  }.property('sortKey')
 });
--- app/controllers/categories/show.js
+++ app/controllers/categories/show.js
@@ -1,6 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	queryParams: {
+		addonSortKey: 'sort'
+	},
+	addonSortKey: 'score:desc',
+
 	hasSubcategories: function() {
 		return this.get('model.subcategories.length') > 0;
 	}.property('model.subcategories')
