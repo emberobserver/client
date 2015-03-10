c63587e46aa542378f15137ffbda69c7f989aa85
Phil Gengler <phil@pgengler.net>
2015-03-09 21:21:59 -0400

    Update deploy script to only copy files if build succeeded

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-ember build --environment production
-scp -r dist/* ember-site@emberobserver.com:/srv/app/ember-site/client/www/
+ember build --environment production && scp -r dist/* ember-site@emberobserver.com:/srv/app/ember-site/client/www/
