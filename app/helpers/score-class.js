0864265150742bfe08f90cc9787e29357c91d8d6
Phil Gengler <phil@pgengler.net>
2015-03-15 22:30:33 -0400

    Show 'WIP' for score when addon has 'isWip' flag set

--- app/helpers/score-class.js
+++ app/helpers/score-class.js
@@ -1,8 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.HTMLBars.makeBoundHelper(function(params) {
-	if (params[0]) {
-		return params[0];
+	let addon = params[0];
+	if (addon.get('isWip')) {
+		return 'wip';
+	}
+	let score = addon.get('score');
+	if (score) {
+		return score;
 	} else {
 		return 'na';
 	}
