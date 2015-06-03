90339304a4e8f52c1067d27ed103ca511256fad5
Phil Gengler <phil@pgengler.net>
2015-06-02 21:51:33 -0400

    Display ember-cli version from an addon's latest version's devDependencies

--- app/models/version.js
+++ app/models/version.js
@@ -6,6 +6,7 @@ var belongsTo = DS.belongsTo;
 export default DS.Model.extend({
   version: attr('string'),
   released: attr('date'),
+  emberCliVersion: attr('string'),
   addon: belongsTo('addon'),
   review: belongsTo('review')
 });
