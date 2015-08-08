952a4d3e74091d4dcb5f3be77a3cd93bf2810e3c
Katie Gengler <katie@kmg.io>
2015-08-08 00:37:38 -0400

    Protect against empty urls

--- app/components/human-url.js
+++ app/components/human-url.js
@@ -3,12 +3,20 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   regex: /\/\/(.*\..*?)(\/.*)/,
   results: function() {
-    return this.get('url').match(this.regex);
+    if (this.get('url')) {
+      return this.get('url').match(this.regex);
+    }
   }.property('url'),
   domain: function() {
-    return this.get('results')[1].replace(/^(www.)?/, '');
+    var results = this.get('results');
+    if (results && results[1]) {
+      return results[1].replace(/^(www.)?/, '');
+    }
   }.property('results'),
   pathname: function() {
-    return this.get('results')[2];
+    var results = this.get('results');
+    if (results && results[2]) {
+      return results[2];
+    }
   }.property('results')
 });
