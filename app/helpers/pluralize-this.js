fbc60f6488e7f08d55bbffbae64bf799299266a5
Katie Gengler <katie@kmg.io>
2015-08-22 13:10:54 -0400

    Switch to our own pluralization helper.

---
+++ app/helpers/pluralize-this.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export function pluralizeThis (params/*, hash*/) {
+  let count = params[0] || 0;
+  let singular = params[1];
+  let plural = params[2] || singular + 's';
+  if (count === 1) { return '1 ' + singular; }
+  return count + ' ' + plural;
+}
+
+export default Ember.Helper.helper(pluralizeThis);
---
+++ tests/unit/helpers/pluralize-this-test.js
@@ -0,0 +1,10 @@
+import { pluralizeThis } from '../../../helpers/pluralize-this';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | pluralize this');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = pluralizeThis(42);
+  assert.ok(result);
+});
