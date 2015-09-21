59ed9bfdbbc78edeb910cefcde34d9ce1be00cdf
Phil Gengler <phil@pgengler.net>
2015-09-20 23:13:08 -0400

    Update username/path for deployment

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ember build --environment production && scp -r dist/* ember-site@emberobserver.com:/srv/app/ember-site/client/www/
+ember build --environment production && scp -r dist/* eo@emberobserver.com:/srv/app/ember-observer/client/www/
