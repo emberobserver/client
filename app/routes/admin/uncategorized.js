662f706f5c5c680063ec453b0563bcc0332fc502
Phil Gengler <phil@pgengler.net>
2015-12-07 11:53:44 -0500

    Fix whitespace to satisfy JSCS

--- app/routes/admin/uncategorized.js
+++ app/routes/admin/uncategorized.js
@@ -1,9 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	model: function() {
-		return this.store.peekAll('addon').filter(function(addon) {
-			return addon.get('categories.length') === 0;
-		});
-	}
+  model: function() {
+    return this.store.peekAll('addon').filter(function(addon) {
+      return addon.get('categories.length') === 0;
+    });
+  }
 });
