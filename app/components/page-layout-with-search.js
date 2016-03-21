8d83508d839672aa13ac2ca2e43dd0e4ca8ed824
Katie Gengler <katie@kmg.io>
2016-03-21 00:47:41 -0400

    Make a component for the layout

--- app/components/page-layout-with-search.js
+++ app/components/page-layout-with-search.js
@@ -1,4 +1,52 @@
 import Ember from 'ember';
+import { task, timeout } from 'ember-concurrency';
+import FocusableComponent from 'ember-component-focus/mixins/focusable-component';
 
-export default Ember.Component.extend({
+export default Ember.Component.extend(FocusableComponent, {
+  store: Ember.inject.service(),
+  session: Ember.inject.service(),
+  searchService: Ember.inject.service('search'),
+  routing: Ember.inject.service('-routing'),
+  focusNode: '#search-input',
+  didInsertElement() {
+    this.focus();
+  },
+  init() {
+    this._super(...arguments);
+    this.get('search').perform(this.get('query'));
+  },
+  categories: Ember.computed(function() {
+    return this.get('store').peekAll('category');
+  }),
+  hasSearchedAndNoResults: Ember.computed('queryIsValid', 'results.length', 'search.isIdle', function() {
+    return this.get('queryIsValid') && !this.get('results.length') && this.get('search.isIdle');
+  }),
+  queryIsValid: Ember.computed('query', function() {
+    let emMatcher = /(^e$|^em$|^emb$|^embe$|^ember$|^ember-$)/;
+    let query = this.get('query');
+    return !(Ember.isBlank(query) || query.length < 3 || emMatcher.test(query));
+  }),
+  search: task(function * (query) {
+    this.set('query', query.trim());
+    if(!this.get('queryIsValid')) {
+      this.set('results', null);
+      return;
+    }
+
+    yield timeout(250);
+
+    let results = yield this.get('searchService').search(query);
+    this.set('results', results);
+  }).restartable(),
+  clearSearch() {
+    this.set('query', '');
+    this.set('results', null);
+    this.focus();
+  },
+  logoutUser() {
+    this.get('session').close().finally(function() {
+      this.get('routing').transitionTo('index');
+    });
+  }
 });
+
