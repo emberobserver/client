0a4ec12b6b58ec9fe9a57fb72165db3b2c82737c
Phil Gengler <phil@pgengler.net>
2016-06-28 09:22:24 -0400

    Get ember-inflector's pluralize() function the ES6 way

--- app/helpers/pluralize.js
+++ app/helpers/pluralize.js
@@ -1,11 +1,12 @@
 import Ember from 'ember';
+import { pluralize } from 'ember-inflector';
 
 export default Ember.Helper.helper(function pluralize(params) {
   let count = params[0] || 0;
   let singular = params[1];
 
   if (count > 1) {
-    return Ember.Inflector.inflector.pluralize(singular);
+    return pluralize(singular);
   }
   return singular;
 });
