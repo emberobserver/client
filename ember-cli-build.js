949ed66b5903b2ae805dc1464698dd86a52f875e
Katie Gengler <katie@kmg.io>
2016-07-08 16:53:18 -0400

    Upgrade to 2.6.x

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,9 +5,6 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
-    babel: {
-      includePolyfill: true,
-    }
   });
 
   // Use `app.import` to add additional libraries to the generated
@@ -22,7 +19,6 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-  app.import('bower_components/typeahead.js/dist/typeahead.bundle.js');
 
   return app.toTree();
 };
