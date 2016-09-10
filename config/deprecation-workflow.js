e0583101f1f4a6c6204131b3ddbc86e1123de8c1
Katie Gengler <katie@kmg.io>
2016-09-10 01:31:09 -0400

    Use ember 2.9 beta

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -1,6 +1,7 @@
 window.deprecationWorkflow = window.deprecationWorkflow || {};
 window.deprecationWorkflow.config = {
   workflow: [
-    { handler: "silence", matchMessage: "Using `ApplicationInstance.container.lookup` is deprecated. Please use `ApplicationInstance.lookup` instead." }
+    { handler: "silence", matchId: "ember-runtime.enumerable-contains" },
+    { handler: "silence", matchId: "ember-htmlbars.ember-handlebars-safestring" }
   ]
 };
