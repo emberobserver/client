c730eacca42771875e3cacd41955cc29091395b9
Michelle Santarsiero <michelle.santarsiero@gmail.com>
2016-12-04 15:21:36 -0500

    Pad line numbers in code search usage to all have same width

---
+++ app/helpers/pad-line-number.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+import {padEnd} from 'ember-pad/utils/pad';
+
+export function padLineNumber(params/*, hash*/) {
+  let numberToPad = params[0];
+  let maxLineNumber = params[1].mapBy('number').pop();
+
+  return padEnd(numberToPad.toString(), maxLineNumber.toString().length, ' ');
+}
+
+export default Ember.Helper.helper(padLineNumber);
