6c9c03686187df6c62acf1af200c6359255403f8
Phil Gengler <phil@pgengler.net>
2017-01-02 21:37:48 -0500

    Add ember-cli-deploy (and plugins) for deploying

---
+++ config/deploy.js
@@ -0,0 +1,31 @@
+/* jshint node: true */
+
+module.exports = function(deployTarget) {
+  var ENV = {
+    build: {}
+    // include other plugin configuration that applies to all deploy targets here
+  };
+
+  ENV['with-rsync'] = {
+    username: 'eo',
+    root: '/srv/app/ember-observer/client/www',
+  };
+
+  if (deployTarget === 'development') {
+    ENV.build.environment = 'development';
+  }
+
+  if (deployTarget === 'staging') {
+    ENV.build.environment = 'production';
+  }
+
+  if (deployTarget === 'production') {
+    ENV.build.environment = 'production';
+    ENV['with-rsync'].host = 'emberobserver.com';
+  }
+
+  // Note: if you need to build some configuration asynchronously, you can return
+  // a promise that resolves with the ENV object instead of returning the
+  // ENV object synchronously.
+  return ENV;
+};
