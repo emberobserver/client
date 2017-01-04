16c4b7d361ef3bebe69127474f3bd3c1a6f9ae4b
Katie Gengler <katie@kmg.io>
2017-01-04 00:46:14 -0500

    Don't pass models in link-to

--- app/routes/addons/correct.js
+++ app/routes/addons/correct.js
@@ -3,6 +3,8 @@ import scrollFix from '../../mixins/scroll-fix';
 
 export default Ember.Route.extend(scrollFix, {
   model(params) {
-    return this.get('store').peekAll('addon').findBy('name', params.name);
+    return this.get('store').query('addon', { filter: { name: params.name }, page: { limit: 1 } }, { reload: true }).then((addons) => {
+      return addons.get('firstObject');
+    });
   }
 });
