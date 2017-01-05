df2fa83c1255f1ef711c671e16611e9af667c424
Michelle Santarsiero <michelle.santarsiero@gmail.com>
2017-01-04 19:42:40 -0500

    Limit number of addon source usages initially shown

--- app/components/addon-source-usages.js
+++ app/components/addon-source-usages.js
@@ -1,12 +1,24 @@
 import Ember from 'ember';
 import { task } from 'ember-concurrency';
 
+const { computed } = Ember;
+
 export default Ember.Component.extend({
+  visibleUsageCount: 25,
+
   showUsages: false,
 
   usages: null,
 
   codeSearch: Ember.inject.service(),
+
+  visibleUsages: computed('visibleUsageCount', 'usages', function() {
+    return this.get('usages').slice(0, this.get('visibleUsageCount'));
+  }),
+
+  moreUsages: computed('visibleUsageCount', 'usages', function() {
+    return this.get('visibleUsageCount') < this.get('usages.length');
+  }),
 
   toggleUsages: task(function* () {
     if (this.get('usages') === null) {
@@ -14,5 +26,12 @@ export default Ember.Component.extend({
       this.set('usages', usages);
     }
     this.toggleProperty('showUsages');
-  }).restartable()
+  }).restartable(),
+
+  actions: {
+    viewMore() {
+      let newUsageCount = this.get('visibleUsageCount') + 25;
+      this.set('visibleUsageCount', newUsageCount);
+    }
+  }
 });
