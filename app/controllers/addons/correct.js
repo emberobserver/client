775b2a2afd9cfda0ca32135b1fb25e10b239eb57
Katie Gengler <katie@kmg.io>
2017-01-21 18:58:47 -0500

    Fix corrections to work with new API

--- app/controllers/addons/correct.js
+++ app/controllers/addons/correct.js
@@ -5,18 +5,18 @@ export default Ember.Controller.extend({
   actions: {
     submitCorrection() {
       let controller = this;
-      Ember.$.post('/api/corrections', {
+      Ember.$.post('/api/v2/corrections', {
         name: this.get('name'),
         email: this.get('email'),
         addon: this.get('model.name'),
         correction: this.get('correction')
       }).done(function() {
-        controller.transitionToRoute('addons.show', controller.get('model'));
+        controller.transitionToRoute('addons.show', controller.get('model.name'));
       });
     },
 
     cancel() {
-      this.transitionToRoute('addons.show', this.get('model'));
+      this.transitionToRoute('addons.show', this.get('model.name'));
     }
   }
 });
