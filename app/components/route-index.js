b5b15945981976a23a8e415cbbf0ea47a5edf1d0
Katie Gengler <katie@kmg.io>
2017-02-11 21:08:18 -0500

    Remove dead code

--- app/components/route-index.js
+++ app/components/route-index.js
@@ -1,14 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  addonSets: Ember.inject.service('addon-sets'),
-  topAddons: Ember.computed(function() {
-    return this.get('addonSets.top').slice(0, 10);
-  }),
-  newAddons: Ember.computed(function() {
-    return this.get('addonSets.newest').slice(0, 10);
-  }),
-  recentlyScoredAddons: Ember.computed(function() {
-    return this.get('addonSets.recentlyReviewed').slice(0, 10);
-  })
 });
