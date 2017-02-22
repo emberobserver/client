4c665a9533c5af39e4f9e2a5a68d945c5cfcae63
Katie Gengler <katie@kmg.io>
2017-02-21 21:56:05 -0500

    Don't include WIP addons in list needing review

--- app/routes/admin/addon-lists/addons-needing-review.js
+++ app/routes/admin/addon-lists/addons-needing-review.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.get('store').query('addon', { filter: { notReviewed: true }, sort: '-latestVersionDate' });
+    return this.get('store').query('addon', { filter: { notReviewed: true, isWip: false }, sort: '-latestVersionDate' });
   }
 });
