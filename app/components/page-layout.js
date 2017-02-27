30c3d1b81940b9058113a7b4db4cda841ece7517
Katie Gengler <katie@kmg.io>
2017-02-27 00:10:47 -0500

    WIP

--- app/components/page-layout.js
+++ app/components/page-layout.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import { task, timeout } from 'ember-concurrency';
 
 const { isBlank } = Ember;
 
@@ -9,13 +10,38 @@ export default Ember.Component.extend({
   session: Ember.inject.service(),
   routing: Ember.inject.service(),
   metrics: Ember.inject.service(),
+  searchService: Ember.inject.service('search'),
   categories: Ember.computed(function() {
     return this.get('store').peekAll('category');
   }),
-  goSearch() {
-    if (!isBlank(this.get('searchTerm'))) {
+  goSearch(term) {
+    if (!isBlank(term)) {
       this.get('metrics').trackEvent({ category: 'Header search', action: `Search on ${document.location.pathname}`, label: this.get('searchTerm') });
-      this.get('routing').transitionTo('index', { queryParams: { query: this.get('searchTerm') } });
+      this.get('routing').transitionTo('index', { queryParams: { query: term } });
+    }
+  },
+  searchForAddons: task(function* (term) {
+    if (term.length === 0) {
+      return Ember.RSVP.resolve([]);
+    }
+
+    yield timeout(250);
+
+    let results = yield this.get('searchService.searchAddonNames').perform(term);
+    let limitedResults = results.slice(0, 5);
+    if (!limitedResults.length) {
+      return limitedResults;
+    }
+
+    limitedResults.insertAt(1, { isFullSearchLink: true });
+    return limitedResults;
+  }).restartable(),
+  goToAddon(selected, options) {
+    if (selected.isFullSearchLink) {
+      this.goSearch(options.searchText);
+    } else {
+      this.set('selectedAddon', selected);
+      this.get('routing').transitionTo('addons.show', selected);
     }
   },
   logoutUser() {
