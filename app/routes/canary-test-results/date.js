100577249606ccafc85b1ee5f9e0b3e77a373393
Phil Gengler <phil@pgengler.net>
2017-03-04 15:16:49 -0500

    Update for JSON API

--- app/routes/canary-test-results/date.js
+++ app/routes/canary-test-results/date.js
@@ -4,7 +4,20 @@ export default Ember.Route.extend({
   model(params) {
     return Ember.RSVP.hash({
       date: params.date,
-      testResults: this.store.query('test-result', { date: params.date, filter: { canary: true } })
+      testResults: this.store.query('test-result', {
+        fields: {
+          'test-result': 'succeeded,status-message,created-at,semver-string,canary'
+        },
+        filter: {
+          canary: true,
+          date: params.date
+        },
+        include: [
+          'ember-version-compatibilities',
+          'version',
+          'version.addon'
+        ].join(',')
+      })
     });
   }
 });
