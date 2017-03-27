a59bacabd2af8e6e319c783e5b6ebca8de9a000d
Christian Spanring <cspanring@gmail.com>
2017-03-27 13:20:27 -0700

    fixes issue with mock api proxy url

--- server/proxies/api.js
+++ server/proxies/api.js
@@ -12,6 +12,6 @@ module.exports = function(app) {
   app.use(proxyPath, function(req, res, next){
     // include root path in proxied request
     req.url = proxyPath + '/' + req.url;
-    proxy.web(req, res, { target: 'http://localhost:3000' });
+    proxy.web(req, res, { target: 'https://emberobserver.com' });
   });
 };
