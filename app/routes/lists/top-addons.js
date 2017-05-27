4d4f80d4ad3c63b8a17b353d6ae090dc14bad0cc
Robert Jackson <rjackson@linkedin.com>
2017-05-27 10:47:07 -0400

    Add `measure` shared utility for ending perf profiling runs.

--- app/routes/lists/top-addons.js
+++ app/routes/lists/top-addons.js
@@ -1,7 +1,10 @@
 import Ember from 'ember';
+import measure from '../../utils/measure';
 
 export default Ember.Route.extend({
   model() {
     return this.get('store').query('addon', { page: { limit: 100 }, filter: { top: true }, sort: 'ranking', include: 'categories' });
-  }
+  },
+
+  afterModel: measure
 });
