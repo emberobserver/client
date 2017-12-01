024230efc743c35fd3dcc692fc332b24e1f63534
Katie Gengler <katie@kmg.io>
2017-12-01 15:31:08 -0500

    Upgrade things

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -2,7 +2,7 @@ import Application from '../../app';
 import config from '../../config/environment';
 import { merge } from '@ember/polyfills';
 import { run } from '@ember/runloop';
-import registerPowerSelectHelpers from '../../tests/helpers/ember-power-select';
+import registerPowerSelectHelpers from 'ember-power-select/test-support/helpers';
 import './qunit-assertions';
 import './mock-analytics';
 import LocalStore from '../../utils/local-storage';
