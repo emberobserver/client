9c142f30af093589c9d905ba01bab23c191d5a72
Katie Gengler <katie@kmg.io>
2017-12-02 22:51:12 -0500

    up timeouts

--- testem.js
+++ testem.js
@@ -4,15 +4,15 @@ module.exports = {
   disable_watching: true,
   "launchers": {
     "bs_ie": {
-      command: "node_modules/.bin/browserstack-launch --os Windows --osv 10 --b ie --bv 11.0 -t 90 -u <url>",
+      command: "node_modules/.bin/browserstack-launch --os Windows --osv 10 --b ie --bv 11.0 -t 600 -u <url>",
       "protocol": "browser"
     },
     "bs_edge": {
-      command: "node_modules/.bin/browserstack-launch --os Windows --osv 10 --b edge --bv 16.0 -t 90 --u <url>",
+      command: "node_modules/.bin/browserstack-launch --os Windows --osv 10 --b edge --bv 16.0 -t 600 --u <url>",
       "protocol": "browser"
     },
     "bs_chrome": {
-      command: "node_modules/.bin/browserstack-launch --os Windows --osv 10 --b chrome --bv 58.0 -t 90 --u <url>",
+      command: "node_modules/.bin/browserstack-launch --os Windows --osv 10 --b chrome --bv 58.0 -t 600 --u <url>",
       "protocol": "browser"
     }
   },
