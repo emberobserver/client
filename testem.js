3e67e87fae41d3fa6de9f7f6a5a1bd711205e0cc
Katie Gengler <katie@kmg.io>
2017-12-02 21:24:32 -0500

    Remove parallel, just in case

--- testem.js
+++ testem.js
@@ -2,7 +2,6 @@
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
-  parallel: 4,
   "launchers": {
     "bs_ie": {
       command: "node_modules/.bin/browserstack-launch --os Windows --osv 10 --b ie --bv 11.0 -t 600 -u <url>",
