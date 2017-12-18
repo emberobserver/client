3a9872c910f578e33839ae6973d8bbd0b87841c4
Katie Gengler <katie@kmg.io>
2017-12-18 16:52:24 -0500

    Use different reporter

--- testem.js
+++ testem.js
@@ -1,6 +1,9 @@
 /* eslint-env node */
+var FailureDotReporter = require('testem-failure-dot-reporter');
+var reporter = new FailureDotReporter();
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
+  reporter: reporter,
   disable_watching: true,
   "launchers": {
     "bs_ie": {
@@ -12,7 +15,7 @@ module.exports = {
       "protocol": "browser"
     },
     "bs_chrome": {
-      command: "node_modules/.bin/browserstack-launch --os Windows --osv 10 --b chrome --bv 58.0 -t 600 --u <url>",
+      command: 'node_modules/.bin/browserstack-launch --os Windows --osv 10 --b chrome --bv 58.0 -t 600 --u "<url>"', // Always quotes around url
       "protocol": "browser"
     }
   },
