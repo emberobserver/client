193f1f1424147bc1556160fe4a37a0559deb5542
Katie Gengler <katie@kmg.io>
2017-12-20 12:43:38 -0500

    Does this reporter way work too?

--- testem.js
+++ testem.js
@@ -1,8 +1,31 @@
+var TapReporter = require('testem/lib/reporters/tap_reporter');
+
+function FailureOnlyReporter() {
+  TapReporter.apply(this, arguments);
+  this._reportCount = 0;
+}
+
+FailureOnlyReporter.prototype = Object.create(TapReporter.prototype);
+FailureOnlyReporter.prototype.constructor = FailureOnlyReporter;
+
+FailureOnlyReporter.prototype.display = function(prefix, result) {
+  this._reportCount++;
+
+  if (!result.passed) {
+    TapReporter.prototype.display.apply(this, arguments);
+  }
+
+  if (this._reportCount > 100) {
+    this.out.write('pass count: ' + this.pass);
+    this._reportCount = 0;
+  }
+};
+
 module.exports = {
   framework: "qunit",
   test_page: "dist/tests/index.html?hidepassed&hideskipped&timeout=60000",
   timeout: 540,
-  tap_quiet_logs: true,
+  reporter: FailureOnlyReporter,
   browser_start_timeout: 400,
   parallel: 4,
   disable_watching: true,
