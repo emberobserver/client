d8a55897a79d1f1bc75b465f1344352658642da9
Katie Gengler <katie@kmg.io>
2017-12-19 22:12:31 -0500

    try parallel

--- testem.js
+++ testem.js
@@ -1,25 +1,27 @@
 /* eslint-env node */
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
+  parallel: 2,
   disable_watching: true,
   "launchers": {
     "bs_ie": {
       command: "node_modules/.bin/browserstack-launch --os Windows --osv 10 --b ie --bv 11.0 -t 600 -u <url>",
       "protocol": "browser"
     },
-    "bs_edge": {
+    bs_edge: {
       exe: "node_modules/.bin/browserstack-launch",
       args: ["--os", "Windows", "--osv", "10", "--b", "edge", "--bv", "16.0", "-t", "600", "--u"],
-      "protocol": "browser"
+      protocol: "browser"
     },
-    "bs_chrome": {
+    bs_chrome: {
       exe: "node_modules/.bin/browserstack-launch",
       args: ["--os", "Windows", "--osv", "10", "--b", "chrome", "--bv", "latest", "-t", "600", "--u"],
-      "protocol": "browser"
+      protocol: "browser"
     }
   },
   "launch_in_dev": [
     "bs_chrome",
+    "bs_edge"
   ],
   "launch_in_ci": [
     "bs_chrome",
