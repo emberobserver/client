1b91e63584959d0881bb754103e5217bfac3f6be
Katie Gengler <katie@kmg.io>
2017-12-20 00:12:42 -0500

    Use same testem.js as ember to see what happens

--- testem.js
+++ testem.js
@@ -1,31 +1,70 @@
-/* eslint-env node */
+var TapReporter = require('testem/lib/reporters/tap_reporter');
+
+function FailureOnlyReporter() {
+  TapReporter.apply(this, arguments);
+  this._reportCount = 0;
+}
+
+FailureOnlyReporter.prototype = Object.create(TapReporter.prototype);
+FailureOnlyReporter.prototype.constructor = FailureOnlyReporter;
+
+FailureOnlyReporter.prototype.display = function(prefix, result) {
+  this._reportCount++;
+
+  if (!result.passed) {
+    TapReporter.prototype.display.apply(this, arguments);
+  }
+
+  if (this._reportCount > 100) {
+    this.out.write('pass count: ' + this.pass);
+    this._reportCount = 0;
+  }
+};
+
 module.exports = {
-  test_page: 'tests/index.html?hidepassed',
-  parallel: 2,
-  browser_start_timeout: 600,
+  framework: "qunit",
+  test_page: "dist/tests/index.html?hidepassed&hideskipped&timeout=60000",
+  timeout: 540,
+  reporter: new FailureOnlyReporter(),
+  browser_start_timeout: 400,
+  parallel: 4,
   disable_watching: true,
-  "launchers": {
-    "bs_ie": {
-      command: "node_modules/.bin/browserstack-launch --os Windows --osv 10 --b ie --bv 11.0 -t 600 -u <url>",
-      "protocol": "browser"
-    },
-    bs_edge: {
+  launchers: {
+    BS_Chrome_Current: {
       exe: "node_modules/.bin/browserstack-launch",
-      args: ["--os", "Windows", "--osv", "10", "--b", "edge", "--bv", "16.0", "-t", "600", "--u"],
+      args: ["--os", "Windows", "--osv", "10", "--b", "chrome", "--bv", "latest", "--u", "<url>"],
       protocol: "browser"
     },
-    bs_chrome: {
+    BS_Firefox_Current: {
       exe: "node_modules/.bin/browserstack-launch",
-      args: ["--os", "Windows", "--osv", "10", "--b", "chrome", "--bv", "latest", "-t", "600", "--u"],
+      args: ["--os", "Windows", "--osv", "10", "--b", "firefox", "--bv", "latest", "--u", "<url>"],
+      protocol: "browser"
+    },
+    BS_Safari_Current: {
+      exe: "node_modules/.bin/browserstack-launch",
+      args: ["--os", "OS X", "--osv", "High Sierra", "--b", "safari", "--bv", "11", "--u", "<url>"],
+      protocol: "browser"
+    },
+    BS_Safari_Last: {
+      exe: "node_modules/.bin/browserstack-launch",
+      args: ["--os", "OS X", "--osv", "Sierra", "--b", "safari", "--bv", "10.1", "--u", "<url>"],
+      protocol: "browser"
+    },
+    BS_MS_Edge: {
+      exe: "node_modules/.bin/browserstack-launch",
+      args: ["--os", "Windows", "--osv", "10", "--b", "edge", "--bv", "latest", "--u", "<url>"],
+      protocol: "browser"
+    },
+    BS_IE_11: {
+      exe: "node_modules/.bin/browserstack-launch",
+      args: ["--os", "Windows", "--osv", "10", "--b", "ie", "--bv", "11.0", "--u", "<url>"],
       protocol: "browser"
     }
   },
-  "launch_in_dev": [
-    "bs_chrome",
-    "bs_edge"
-  ],
-  "launch_in_ci": [
-    "bs_chrome",
-    "bs_edge"
+  launch_in_dev: [],
+  launch_in_ci: [
+    "BS_Safari_Current",
+    "BS_MS_Edge",
+    "BS_IE_11"
   ]
 };
