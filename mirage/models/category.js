6901893cef6d8548a351b05ee81412efbca2569d
Katie Gengler <katie@kmg.io>
2018-02-17 16:16:10 -0500

    Upgrade ember-cli-mirage

--- mirage/models/category.js
+++ mirage/models/category.js
@@ -1,7 +1,7 @@
 import { Model, hasMany, belongsTo } from 'ember-cli-mirage';
 
 export default Model.extend({
-  parent: belongsTo('category'),
-  subcategories: hasMany('category'),
+  parent: belongsTo('category', { inverse: 'subcategories' }),
+  subcategories: hasMany('category', { inverse: 'parent' }),
   addons: hasMany('addon')
 });
