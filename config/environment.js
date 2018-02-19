2cf096997105fdf994489fa37ba7054877d7cfc9
Phil Gengler <phil@pgengler.net>
2018-02-18 22:01:01 -0500

    Let 'ember-versions-model' feature flag default to false instead of setting it to false

--- config/environment.js
+++ config/environment.js
@@ -74,7 +74,6 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
-    ENV.featureFlags['ember-versions-model'] = false;
   }
 
   return ENV;
