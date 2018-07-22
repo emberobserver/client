3a718d02c9bd101d432cc3b660e76a69250726b7
Katie Gengler <katie@kmg.io>
2018-07-22 19:40:03 -0400

    Fix save button styling (caused by dup attribute: class)
    - Add more template-lint rules + fix up to catch this kind of thing in
    the future

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -2,5 +2,18 @@
 'use strict';
 
 module.exports = {
-  extends: 'recommended'
+  extends: 'recommended',
+  rules: {
+    'eol-last': 'always',
+    'inline-link-to': true,
+    'no-inline-styles': true,
+    'no-duplicate-attributes': true,
+    'no-input-block': true,
+    'no-input-tagname': true,
+    'no-unbound': true,
+    'no-outlet-outside-routes': true,
+    'no-trailing-spaces': true,
+    'no-partial': true,
+    // 'quotes': 'double',
+  }
 };
