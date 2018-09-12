76c9dc0ef6fc8d8f4a27dbe826bb480e958d1f41
Katie Gengler <katie@kmg.io>
2018-09-12 02:08:13 -0400

    Remove deprecations no longer relevant

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -1,8 +1,6 @@
 window.deprecationWorkflow = window.deprecationWorkflow || {};
 window.deprecationWorkflow.config = {
   workflow: [
-    { handler: "silence", matchId: "ember-env.old-extend-prototypes" }, // ember-data
-    { handler: "silence", matchId: "ember-inflector.globals" }, // ember-data
     { handler: "silence", matchId: "ember-routing.route-router" }, // ember-cli-document-title
     { handler: "silence", matchId: "ember-console.deprecate-logger" }, // ember-moment
   ]
