554a5a3ea479dc1f35cb99d13fb5c08f262dad32
Michelle Santarsiero <michelle.santarsiero@gmail.com>
2018-11-10 17:03:00 -0500

    Move data fetching and messages into dependency-table component

--- app/components/dependency-table.js
+++ app/components/dependency-table.js
@@ -1,10 +1,31 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
+import { task } from 'ember-concurrency';
+import { isEmpty } from '@ember/utils';
 
 export default Component.extend({
   limit: 8,
 
   showingAllDependencies: false,
+
+  store: service(),
+
+  init() {
+    this._super(...arguments);
+    this.get('fetchDependencies').perform();
+  },
+
+  fetchDependencies: task(function * () {
+    let addonVersionId = this.get('addonVersion.id');
+    let results = yield this.store.query('addon-dependency', { filter: { addonVersionId }, sort: 'package' });
+    this.set('dependencies',  results.filter((dep) => dep.isDependency));
+    this.set('devDependencies',  results.filter((dep) => dep.isDevDependency));
+  }),
+
+  addonHasDependencies: computed('dependencies', 'devDependencies', function() {
+    return !isEmpty(this.dependencies) || !isEmpty(this.devDependencies);
+  }),
 
   dependencyCount: computed.alias('dependencies.length'),
 
