c9af20a9adf5a83058186b7fb6f74805cde3c0d1
Edward Faulkner <ef@alum.mit.edu>
2019-03-18 09:08:01 -0700

    adding embroider

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -61,5 +61,65 @@ module.exports = function(defaults) {
     ]
   });
 
-  return app.toTree();
+  if (process.env.CLASSIC) {
+    return app.toTree();
+  }
+
+  const Webpack = require('@embroider/webpack').Webpack;
+  return require('@embroider/compat').compatBuild(app, Webpack, {
+
+    // The default settings give maximum compatability, and they work fine here,
+    // but we are also going to enable some optimizations.
+
+    // These two flags are used when building classic addons to v2 format. They
+    // cause any unused Javascript modules these addon trees to not be included
+    // in the build. Most addons are fine with this, but some are not, so this
+    // is an opt-in optmization.
+    staticAddonTestSupportTrees: true,
+    staticAddonTrees: true,
+
+    // Resolve all helpers (in app and all addons) at build time. Unused helpers
+    // will not be included. This is usually a safe optimization, as there are
+    // not common ways of dynamically referring to helpers in templates.
+    staticHelpers: true,
+
+    // Resolve all components (in app and all addons) at build time. Unused
+    // components will not be included. This is generally not a safe
+    // optimization unless you also deal with any dynamic component warnings by
+    // providing packageRules.
+    staticComponents: true,
+
+    // The packageRules system allows you to tell Embroider about sources of
+    // dynamic dependencies in classic addons and apps that are otherwise not
+    // statically analyzable.
+    packageRules: [
+
+      // You can have entries for any package, meaning apps and addons. This one
+      // is for our own app. There are some packageRules that ship inside
+      // Embroider for common addons, which you will get by default if you don't
+      // override them in here.
+      {
+        package: 'ember-observer',
+        components: {
+
+          // this tells embroider not to warn us about:
+          //
+          //  {{#exclusive-button-group as |btn|}}
+          //    {{component btn}}
+          //  {{/exclusive-button-group}}
+          //
+          //
+          // It is safe in this case because exclusive-button-group's
+          // implementation does:
+          //
+          //   {{yield (component "exclusive-button" ...) }}
+          //
+          // which is a statically-analyzable source for the btn component.
+          '{{exclusive-button-group}}': {
+            yieldsSafeComponents: [ true ]
+          }
+        }
+      }
+    ]
+  });
 };
