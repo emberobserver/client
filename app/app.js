72f50f31cacff761d2a499fbd051fdb3a7ddcee5
Edward Faulkner <ef@alum.mit.edu>
2019-03-18 16:55:45 -0700

    fixing ember-metrics
    Two things were broken with ember-metrics.
    
    First, it does dynamic container lookup of its adapters, which I'm working around here by explicitly importing and defining the ones we need. (A way to do this directly from packageRules in embroider will be forthcoming.)
    
    Second, the addon itself actually contains invalid usage of import & export. See https://github.com/poteto/ember-metrics/pull/221

--- app/app.js
+++ app/app.js
@@ -5,6 +5,12 @@ import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
+import GoogleAnalytics from 'ember-metrics/metrics-adapters/google-analytics';
+window.define('ember-metrics/metrics-adapters/google-analytics', function(){ return GoogleAnalytics; });
+import LocalAnalytics from './metrics-adapters/local-adapter';
+window.define('ember-observer/metrics-adapters/local-adapter', function(){ return LocalAnalytics; });
+
+
 const App = Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
