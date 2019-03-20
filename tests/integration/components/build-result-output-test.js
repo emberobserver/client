a0acdc22094c20bd2e13dcac96d40f8d8cd012ff
Phil Gengler <phil@pgengler.net>
2019-03-20 11:12:03 -0700

    Improved JSON handling

--- tests/integration/components/build-result-output-test.js
+++ tests/integration/components/build-result-output-test.js
@@ -15,19 +15,89 @@ module('Integration | Component | build-result-output', function(hooks) {
     assert.dom('pre').hasText("I'm a teapot!", 'renders output in a <pre> tag');
   });
 
-  test('renders output that is valid JSON', async function(assert) {
-    this.set('buildResult', {
-      output: JSON.stringify([
-        {
-          group: 'Group One',
-          commands: [
-            { command: 'ls', output: 'foo' },
-          ]
-        }
-      ])
-    });
-    await render(hbs`<BuildResultOutput @buildResult={{buildResult}} />`);
+  module('JSON-format test output', function() {
+    test('renders command groups', async function(assert) {
+      this.set('buildResult', {
+        output: JSON.stringify([
+          {
+            group: 'Group One',
+            commands: [
+              { command: 'ls', output: 'foo' },
+            ]
+          },
+          {
+            group: 'Group Two',
+            commands: [
+              { command: 'rmdir foo', output: 'rmdir: foo: Not a directory' }
+            ]
+          }
+        ])
+      });
+      await render(hbs`<BuildResultOutput @buildResult={{buildResult}} />`);
 
-    assert.dom(this.element).hasText('Hi');
+      assert.dom('section').exists({ count: 2 }, 'renders <section> tags for each group');
+    });
+
+    test('displays command group details', async function(assert) {
+      this.set('buildResult', {
+        output: JSON.stringify([
+          {
+            group: 'Group One',
+            commands: [
+              { command: 'ls', output: 'foo' },
+            ]
+          }
+        ])
+      });
+      await render(hbs`<BuildResultOutput @buildResult={{buildResult}} />`);
+
+      assert.dom('section > details > summary').hasText('Group One', 'displays group header');
+      assert.dom('section > details > details > summary').hasText('ls', 'displays command in a <summary>');
+      assert.dom('section > details > details > output').hasText('foo', 'displays command output as an <output>');
+    });
+
+    test('command element displays as open when command was not successful', async function(assert) {
+      this.set('buildResult', {
+        output: JSON.stringify([
+          {
+            group: 'Group One',
+            commands: [
+              { command: 'ls foo', output: 'ls: foo: No such file or directory', failed: true }
+            ]
+          }
+        ])
+      });
+      await render(hbs`<BuildResultOutput @buildResult={{buildResult}} />`);
+
+      assert.dom('section > details > details').hasAttribute('open');
+    });
+
+    test('when a group has has_failures: true, that group is open by default', async function(assert) {
+      this.set('buildResult', {
+        output: JSON.stringify([
+          {
+            group: 'Group One',
+            has_failures: true
+          }
+        ])
+      });
+      await render(hbs`<BuildResultOutput @buildResult={{buildResult}} />`);
+
+      assert.dom('section > details').hasAttribute('open');
+    });
+
+    test('when a group has has_failures: false, that group is not open by default', async function(assert) {
+      this.set('buildResult', {
+        output: JSON.stringify([
+          {
+            group: 'Group One',
+            has_failures: false
+          }
+        ])
+      });
+      await render(hbs`<BuildResultOutput @buildResult={{buildResult}} />`);
+
+      assert.dom('section > details').doesNotHaveAttribute('open');
+    });
   });
 });
