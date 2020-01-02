f925504801cae474a6d1f100cc1daed37d5c5a17
Katie Gengler <katie@kmg.io>
2020-01-01 23:37:44 -0500

    Add an application serializer to get rid of ember-data deprecation

--- app/serializers/review.js
+++ app/serializers/review.js
@@ -1,6 +1,6 @@
-import DS from 'ember-data';
+import ApplicationSerializer from './application';
 
-export default DS.JSONAPISerializer.extend({
+export default ApplicationSerializer.extend({
   attrs: {
     createdAt: { serialize: false }
   }
