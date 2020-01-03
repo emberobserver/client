54bf83a1bfd0c06597a11ba35d2c7ec96d459394
Katie Gengler <katie@kmg.io>
2020-01-02 23:33:33 -0500

    Enable no-action rule, add pending to template-lint config to prevent new usages of {{action

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,6 +1,176 @@
 'use strict';
 
 module.exports = {
+  pending: [
+    {
+      "moduleId": "app/templates/login",
+      "only": [
+        "no-action"
+      ]
+    },
+    {
+      "moduleId": "app/templates/addons/correct",
+      "only": [
+        "no-action"
+      ]
+    },
+    {
+      "moduleId": "app/templates/admin/build-servers",
+      "only": [
+        "no-action"
+      ]
+    },
+    {
+      "moduleId": "app/templates/admin/review",
+      "only": [
+        "no-action"
+      ]
+    },
+    {
+      "moduleId": "app/templates/components/addon-review-form",
+      "only": [
+        "no-action"
+      ]
+    },
+    {
+      "moduleId": "app/templates/components/addon-source-usages",
+      "only": [
+        "no-action"
+      ]
+    },
+    {
+      "moduleId": "app/templates/components/addon-version-list",
+      "only": [
+        "no-action"
+      ]
+    },
+    {
+      "moduleId": "app/templates/components/admin-addon-review-form",
+      "only": [
+        "no-action"
+      ]
+    },
+    {
+      "moduleId": "app/templates/components/admin-addon",
+      "only": [
+        "no-action"
+      ]
+    },
+    {
+      "moduleId": "app/templates/components/code-search",
+      "only": [
+        "no-action"
+      ]
+    },
+    {
+      "moduleId": "app/templates/components/dependency-table",
+      "only": [
+        "no-action"
+      ]
+    },
+    {
+      "moduleId": "app/templates/components/dependency-tables",
+      "only": [
+        "no-action"
+      ]
+    },
+    {
+      "moduleId": "app/templates/components/ember-version-compatibility",
+      "only": [
+        "no-action"
+      ]
+    },
+    {
+      "moduleId": "app/templates/components/exclusive-button",
+      "only": [
+        "no-action"
+      ]
+    },
+    {
+      "moduleId": "app/templates/components/large-search",
+      "only": [
+        "no-action"
+      ]
+    },
+    {
+      "moduleId": "app/templates/components/login-form",
+      "only": [
+        "no-action"
+      ]
+    },
+    {
+      "moduleId": "app/templates/components/page-layout",
+      "only": [
+        "no-action"
+      ]
+    },
+    {
+      "moduleId": "app/templates/components/radio-button-set",
+      "only": [
+        "no-action"
+      ]
+    },
+    {
+      "moduleId": "app/templates/components/radio-button",
+      "only": [
+        "no-action"
+      ]
+    },
+    {
+      "moduleId": "app/templates/components/score-detail",
+      "only": [
+        "no-action"
+      ]
+    },
+    {
+      "moduleId": "app/templates/components/search-result-set",
+      "only": [
+        "no-action"
+      ]
+    },
+    {
+      "moduleId": "app/templates/components/sort-option",
+      "only": [
+        "no-action"
+      ]
+    },
+    {
+      "moduleId": "app/templates/components/sort-order-option",
+      "only": [
+        "no-action"
+      ]
+    },
+    {
+      "moduleId": "app/templates/components/stats-bar",
+      "only": [
+        "no-action"
+      ]
+    },
+    {
+      "moduleId": "app/templates/components/toggle-switch",
+      "only": [
+        "no-action"
+      ]
+    },
+    {
+      "moduleId": "app/templates/admin/build-results/show",
+      "only": [
+        "no-action"
+      ]
+    },
+    {
+      "moduleId": "app/templates/admin/categories/edit",
+      "only": [
+        "no-action"
+      ]
+    },
+    {
+      "moduleId": "app/templates/admin/categories/new",
+      "only": [
+        "no-action"
+      ]
+    }
+  ],
   extends: 'octane',
   rules: {
     'eol-last': 'always',
@@ -15,6 +185,5 @@ module.exports = {
     'no-partial': true,
     'quotes': false,
     'table-groups': false,
-    'no-action': false,
-  }
+  },
 };
