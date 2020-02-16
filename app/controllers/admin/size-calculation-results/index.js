edd403285b77ede6b49f0e2e1f095a2b2deaff38
Michelle Santarsiero <michelle.santarsiero@gmail.com>
2020-02-16 14:59:37 -0500

    Update to use native classes

--- app/controllers/admin/size-calculation-results/index.js
+++ app/controllers/admin/size-calculation-results/index.js
@@ -1,8 +1,12 @@
+import classic from 'ember-classic-decorator';
 import { sort } from '@ember/object/computed';
 import Controller from '@ember/controller';
 
-export default Controller.extend({
-  queryParams: ['date'],
-  sizeCalculationResultSorting: ['testsRunAt:desc'],
-  sortedSizeCalculationResults: sort('model', 'sizeCalculationResultSorting'),
-});
+@classic
+export default class SizeCalculationResultsIndexController extends Controller {
+  queryParams = ['date'];
+  sizeCalculationResultSorting = ['testsRunAt:desc'];
+
+  @sort('model', 'sizeCalculationResultSorting')
+  sortedSizeCalculationResults;
+}
--- app/controllers/admin/size-calculation-results/show.js
+++ app/controllers/admin/size-calculation-results/show.js
@@ -1,33 +1,44 @@
-import { computed } from '@ember/object';
+import classic from 'ember-classic-decorator';
+import { action, computed } from '@ember/object';
 import { inject as service } from '@ember/service';
 import Controller from '@ember/controller';
-import { alias } from '@ember/object/computed';
+import { readOnly } from '@ember/object/computed';
 
-export default Controller.extend({
-  api: service('api'),
-  sizeCalculationResult: alias('model'),
-  addonVersion: alias('sizeCalculationResult.version'),
-  addon: alias('addonVersion.addon'),
-  hasRetriedBuild: false,
+@classic
+export default class SizeCalculationResultsShowController extends Controller {
+  @service
+  api;
 
-  buildStatus: computed('sizeCalculationResult.succeeded', 'sizeCalculationResult.errorMessage', function() {
+  @readOnly('model')
+  sizeCalculationResult;
+
+  @readOnly('sizeCalculationResult.version')
+  addonVersion;
+
+  @readOnly('addonVersion.addon')
+  addon;
+
+  hasRetriedBuild = false;
+
+  @computed('sizeCalculationResult.succeeded', 'sizeCalculationResult.errorMessage')
+  get buildStatus() {
     if (this.get('sizeCalculationResult.succeeded')) {
       return 'succeeded';
     }
     return this.get('sizeCalculationResult.errorMessage');
-  }),
-
-  canRetryBuild: computed('sizeCalculationResult.succeeded', 'hasRetriedBuild', function() {
-    return !this.get('sizeCalculationResult.succeeded') && !this.get('hasRetriedBuild');
-  }),
-
-  actions: {
-    retryBuild() {
-      this.set('hasRetriedBuild', true);
-      this.api.request(`/size_calculation_results/${this.get('sizeCalculationResult.id')}/retry`, { method: 'POST' }).catch((e) => {
-        this.get('hasRetriedBuild', false);
-        throw e;
-      });
-    }
   }
-});
+
+  @computed('sizeCalculationResult.succeeded', 'hasRetriedBuild')
+  get canRetryBuild() {
+    return !this.get('sizeCalculationResult.succeeded') && !this.get('hasRetriedBuild');
+  }
+
+  @action
+  retryBuild() {
+    this.set('hasRetriedBuild', true);
+    this.api.request(`/size_calculation_results/${this.get('sizeCalculationResult.id')}/retry`, { method: 'POST' }).catch((e) => {
+      this.get('hasRetriedBuild', false);
+      throw e;
+    });
+  }
+}
