93458f7be97c7027e1eceee28d87e83997b1c668
Michelle Santarsiero <michelle.santarsiero@gmail.com>
2020-03-07 08:06:00 -0500

    glimmer-ify date nav

--- app/components/date-nav.js
+++ app/components/date-nav.js
@@ -1,34 +1,24 @@
-import classic from 'ember-classic-decorator';
-import Component from '@ember/component';
-import { computed } from '@ember/object';
+import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
 import moment from 'moment';
 
-@classic
 export default class DateNavComponent extends Component {
-  tagName = 'section';
+  @tracked date;
 
-  classNames = ['date-nav'];
-
-  @computed('date')
   get formattedDisplayDate() {
-    return moment(this.get('date')).utc().format('YYYY-MM-DD');
+    return moment(this.args.date).utc().format('YYYY-MM-DD');
   }
 
-  @computed('date')
   get formattedPreviousDate() {
-    let date = this.get('date');
-    return moment(date).subtract(1, 'day').format('YYYY-MM-DD');
+    return moment(this.args.date).subtract(1, 'day').format('YYYY-MM-DD');
   }
 
-  @computed('date')
   get formattedFollowingDate() {
-    let date = this.get('date');
-    return moment(date).add(1, 'day').format('YYYY-MM-DD');
+    return moment(this.args.date).add(1, 'day').format('YYYY-MM-DD');
   }
 
-  @computed('date')
   get showFollowingDayLink() {
-    let dateFromParam = moment(this.get('date'));
+    let dateFromParam = moment(this.args.date);
     let currentDate = moment();
     return !dateFromParam.isSame(currentDate, 'day');
   }
