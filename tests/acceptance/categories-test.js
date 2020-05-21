291f7ff00329e9ab51952b20ee153986f91fc5fa
Phil Gengler <phil@pgengler.net>
2020-05-21 14:40:01 -0400

    Fix sorting for categories
    Add a test (which we didn't have) and convert the <SortOption/> component to be a Glimmer component

--- tests/acceptance/categories-test.js
+++ tests/acceptance/categories-test.js
@@ -1,7 +1,6 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { click, currentURL, findAll, visit } from '@ember/test-helpers';
 import { setupEmberObserverTest } from '../helpers/setup-ember-observer-test';
-
 
 module('Acceptance | categories', function(hooks) {
   setupEmberObserverTest(hooks);
@@ -11,5 +10,32 @@ module('Acceptance | categories', function(hooks) {
 
     assert.equal(currentURL(), '/categories/addons-that-violate-fundamental-laws-of-the-universe', 'preserves URL');
     assert.dom('.test-not-found').hasText("Oops! We can't find what you were looking for. Try searching above?");
+  });
+
+  test('can change category sorting', async function(assert) {
+    server.logging = true;
+    let category = server.create('category', { name: 'Awesome addons' });
+    server.create('addon', {
+      categories: [category],
+      name: 'Omega',
+      score: 10,
+    });
+    server.create('addon', {
+      categories: [category],
+      name: 'Alpha',
+      score: 5,
+    });
+    await visit('/categories/awesome-addons');
+
+    // addons are initially sorted by score
+    assert.dom(findAll('.test-addon-name .addon-name')[0]).hasText('Omega');
+    assert.dom(findAll('.test-addon-name .addon-name')[1]).hasText('Alpha');
+
+    // change to sort by name
+    await click('[data-test-sort-by="name"]');
+
+    // addons are now sorted by name
+    assert.dom(findAll('.test-addon-name .addon-name')[0]).hasText('Alpha');
+    assert.dom(findAll('.test-addon-name .addon-name')[1]).hasText('Omega');
   });
 });
