ceb757d4fb5c47ca8789d66d1972a05c166896e1
Phil Gengler <phil@pgengler.net>
2020-05-21 14:40:01 -0400

    Fix sorting for categories
    Add a test (which we didn't have) and convert the <SortOption/> component to be a Glimmer component

--- app/components/sort-option.js
+++ app/components/sort-option.js
@@ -1,13 +1,7 @@
-import classic from 'ember-classic-decorator';
-import { tagName } from '@ember-decorators/component';
-import { computed } from '@ember/object';
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 
-@classic
-@tagName('')
 export default class SortOption extends Component {
-  @computed('selectedSort', 'key')
   get isSelected() {
-    return this.selectedSort === this.key;
+    return this.args.selectedSort === this.args.key;
   }
 }
