f9941983cb210af1fdb144b955ad09000a80b31f
Phil Gengler <phil@pgengler.net>
2020-10-31 16:50:09 -0400

    Convert <AddonVersionList/> to be a Glimmer component

--- app/components/addon-version-list.js
+++ app/components/addon-version-list.js
@@ -1,47 +1,44 @@
-import classic from 'ember-classic-decorator';
-import { action, computed } from '@ember/object';
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
-import { gt } from '@ember/object/computed';
-import Component from '@ember/component';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 
-@classic
 export default class AddonVersionList extends Component {
-  showAll = false;
+  @tracked showAll = false;
 
   @service
   emberVersions;
 
-  @computed('versions', 'showAll')
   get showingVersions() {
     if (this.showAll) {
-      return this.versions;
+      return this.args.versions;
     }
-    return (this.versions || []).slice(0, 10);
+    return (this.args.versions || []).slice(0, 10);
   }
 
-  @computed('emberVersions.versions.[]', 'showingVersions.lastObject')
   get emberVersionDataAfterOldestShowingAddonVersion() {
-    let oldestVersionDate = this.get('showingVersions.lastObject.released');
-    return this.get('emberVersions.versions').filter(version => version.released > oldestVersionDate);
+    let oldestVersion = this.showingVersions[this.showingVersions.length - 1];
+    let oldestVersionDate = oldestVersion ? oldestVersion.released : null;
+    return this.emberVersions.versions.filter(version => version.released > oldestVersionDate);
   }
 
-  @computed('emberVersionDataAfterOldestShowingAddonVersion.[]', 'showingVersions.[]')
   get versionsWithMeta() {
     let versions = this.emberVersionDataAfterOldestShowingAddonVersion.map(version => ({ isEmber: true, version }));
     versions = versions.concat(this.showingVersions.map(version => ({ isAddon: true, version })));
     return versions.sortBy('version.released').reverse();
   }
 
-  @gt('versions.length', 10)
-  moreThan10Versions;
+  get moreThan10Versions() {
+    return this.args.versions.length > 10;
+  }
 
-  @computed('moreThan10Versions', 'showAll')
   get thereAreHiddenVersions() {
     return this.moreThan10Versions && !this.showAll;
   }
 
   @action
-  showAllVersions() {
-    this.set('showAll', true);
+  showAllVersions(event) {
+    event.preventDefault();
+    this.showAll = true;
   }
 }
