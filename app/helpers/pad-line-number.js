2390d78e9db4cc6d9259b2dda78476486320b432
Phil Gengler <phil@pgengler.net>
2020-10-31 15:00:25 -0400

    Remove ember-pad dependency, using String.prototype.padEnd instead

--- app/helpers/pad-line-number.js
+++ app/helpers/pad-line-number.js
@@ -1,11 +1,9 @@
 import { helper } from '@ember/component/helper';
-import { padEnd } from 'ember-pad/utils/pad';
 
-export function padLineNumber(params/* , hash*/) {
-  let [numberToPad] = params;
-  let maxLineNumber = params[1].mapBy('number').pop();
+export function padLineNumber([numberToPad, lines]) {
+  let maxLineNumber = lines.mapBy('number').pop();
 
-  return padEnd(numberToPad.toString(), maxLineNumber.toString().length, ' ');
+  return numberToPad.toString().padEnd(maxLineNumber.toString().length, ' ');
 }
 
 export default helper(padLineNumber);
