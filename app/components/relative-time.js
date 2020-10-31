78955d4b8c04c7f17ac571efe161949e62d5ca71
Phil Gengler <phil@pgengler.net>
2020-10-31 13:53:32 -0400

    Make <RelativeTime/> use currentDate service

--- app/components/relative-time.js
+++ app/components/relative-time.js
@@ -1,6 +1,9 @@
+import { inject as service } from '@ember/service';
 import Component from '@glimmer/component';
 
 export default class RelativeTime extends Component {
+  @service currentDate;
+
   get isoDate() {
     let date = this.args.date;
     return date ? date.toISOString() : null;
--- app/services/current-date.js
+++ app/services/current-date.js
@@ -1,7 +1,8 @@
 import Service from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
 export default class CurrentDateService extends Service {
-  currentDateOverride = null;
+  @tracked currentDateOverride = null;
 
   get date() {
     return this.currentDateOverride || new Date();
