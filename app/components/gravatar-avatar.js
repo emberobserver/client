f76b2720c1d0190e84ad4d9d49bdd4ca3ccfa275
Phil Gengler <phil@pgengler.net>
2020-11-01 15:17:29 -0500

    Convert <GravatarAvatar/> to be a Glimmer component

--- app/components/gravatar-avatar.js
+++ app/components/gravatar-avatar.js
@@ -1,15 +1,8 @@
-import classic from 'ember-classic-decorator';
-import { attributeBindings, tagName } from '@ember-decorators/component';
-import { computed } from '@ember/object';
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 
-@classic
-@tagName('img')
-@attributeBindings('src', 'title', 'alt')
 export default class GravatarAvatar extends Component {
-  @computed('gravatarId')
   get src() {
-    let gravatarId = this.gravatarId || '';
+    let gravatarId = this.args.gravatarId || '';
     return `https://secure.gravatar.com/avatar/${gravatarId}?d=identicon`;
   }
 }
