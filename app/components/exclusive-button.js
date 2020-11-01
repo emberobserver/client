3583ad1aab1d4c1a683763514f0310ca359810df
Phil Gengler <phil@pgengler.net>
2020-11-01 09:40:12 -0500

    Convert <ExclusiveButton/> to be a Glimmer component

--- app/components/exclusive-button.js
+++ app/components/exclusive-button.js
@@ -1,12 +1,10 @@
-import classic from 'ember-classic-decorator';
-import { tagName } from '@ember-decorators/component';
-import Component from '@ember/component';
+import { action } from '@ember/object';
+import Component from '@glimmer/component';
 
-@classic
-@tagName('')
 export default class ExclusiveButton extends Component {
-  label = '';
-  value = null;
-  selectedValue = null;
-  updateSelectedValue = null;
+  @action
+  updateSelectedValue(event) {
+    event.preventDefault();
+    this.args.updateSelectedValue(this.args.value);
+  }
 }
