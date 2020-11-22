48b96ce892ab250a0fcf09e4dc18be7f15585924
Michelle Santarsiero <michelle.santarsiero@gmail.com>
2020-11-22 16:46:35 -0500

    Sort size calculations by addon name then most recent

--- app/controllers/admin/size-calculation-results/index.js
+++ app/controllers/admin/size-calculation-results/index.js
@@ -1,12 +1,26 @@
 import classic from 'ember-classic-decorator';
-import { sort } from '@ember/object/computed';
+import { computed } from '@ember/object';
 import Controller from '@ember/controller';
 
 @classic
 export default class SizeCalculationResultsIndexController extends Controller {
   queryParams = ['date'];
-  sizeCalculationResultSorting = ['testsRunAt:desc'];
 
-  @sort('model', 'sizeCalculationResultSorting')
-  sortedSizeCalculationResults;
+  @computed('model')
+  get sortedSizeCalculationResults() {
+    return this.model.toArray().sort(sortByAddonNameAndMostRecent);
+  }
+}
+
+function sortByAddonNameAndMostRecent(a, b) {
+  let addonName1 = a.get('version.addonName');
+  let addonName2 = b.get('version.addonName');
+
+  if (addonName1 < addonName2) {
+    return -1;
+  }
+  if (addonName1 > addonName2) {
+    return 1;
+  }
+  return b.get('createdAt') - a.get('createdAt');
 }
